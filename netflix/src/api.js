f430bb67545725e60d549a6d205d21f6c96dabb4
Janisson <janisson.bass@gmail.com>
2022-10-09 20:20:23 -0300

    initial commit

---
+++ netflix/src/api.js
@@ -0,0 +1,9 @@
+const API_KEY = "2b651dbb9aca74253ec79182a48d3297";
+
+const categories =[
+    {
+        name: "trending",
+        title:"Em alta",
+        path:`/trending/all/week?api_key=${API_KEY}&language=pt-BR`
+    }
+]
