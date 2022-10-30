81aab93c845daa08b847adcfec578ebda0180647
Janisson <janisson.bass@gmail.com>
2022-10-30 20:06:28 -0300

    finalizando cline basico

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Row from './components/Row';
+
+import categories from './api';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-   
+      <Nav />
+      <Banner />
+      {categories.map((category)=>{
+        return <Row key={category.name} title={category.title} path={category.path} isLarge={category.isLarge}/>
+      })}
+      
     </div>
   );
 }
--- netflix/src/api.js
+++ netflix/src/api.js
@@ -4,6 +4,51 @@ const categories =[
     {
         name: "trending",
         title:"Em alta",
-        path:`/trending/all/week?api_key=${API_KEY}&language=pt-BR`
+        path:`/trending/all/week?api_key=${API_KEY}&language=pt-BR`,
+        isLarge:true
+    },
+    {
+        name: "netflixOriginals",
+        title:"Originais Netflix",
+        path:`/discover/tv?api_key=${API_KEY}&with_networks=123`,
+        isLarge:false
+    },
+    {
+        name: "topRated",
+        title:"Populares",
+        path:`/movie/top_rated?api_key=${API_KEY}&language=pt-BR`,
+        isLarge:false
+    },
+    {
+        name: "comedy",
+        title:"Comédias",
+        path:`/discover/tv?api_key=${API_KEY}&with_genres=35`,
+        isLarge:false
+    },
+    {
+        name: "romances",
+        title:"Romances",
+        path:`/discover/tv?api_key=${API_KEY}&with_genres=1074`,
+        isLarge:false
+    },
+    {
+        name: "documentaries",
+        title:"Documantários",
+        path:`/discover/tv?api_key=${API_KEY}&with_genres=99`,
+        isLarge:false
+    },
+];
+
+export const getmovies = async (path) => {
+    try{
+        let url = `https://api.themoviedb.org/3${path}`;
+        const response = await fetch(url);
+        return await response.json();
+    }catch (error){
+        console.log(`Erro getMovies: ${error}`);
     }
-]
+}
+
+
+
+export default categories;
